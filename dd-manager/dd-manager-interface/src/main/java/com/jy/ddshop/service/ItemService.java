1102ce1dbbca9b4999f42a0922b74f530c2ee188
jiangyanjp <2460311408@qq.com>
2017-11-07 22:47:08 +0800

    easyui之选项卡

---
+++ dd-manager/dd-manager-interface/src/main/java/com/jy/ddshop/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.jy.ddshop.service;
+
+import com.jy.ddshop.pojo.po.TbItem;
+
+/**
+ * User: DHC
+ * Date: 2017/11/6
+ * Time: 9:48
+ * Version:V1.0
+ */
+public interface ItemService {
+    TbItem getById(Long itemId);
+}
